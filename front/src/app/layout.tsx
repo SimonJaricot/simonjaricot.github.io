4ca312a4a16070998b1f584499bc20c1477659ef
SimonJaricot <simon.jaricot@protonmail.com>
2025-09-12 11:23:56 +0200

    chore: refactor some stuff

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -7,11 +7,6 @@ const geistSans = Geist({
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
-
 const firaCode = Fira_Code({
   variable: "--font-fira-code",
   subsets: ["latin"],
